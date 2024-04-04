1e1906d3464ee5b691efec6602936a245a781116
baktima <136801302+baktima@users.noreply.github.com>
2024-04-04 15:18:31 +0800

    Delete Documents directory

--- Documents/c projects/test/hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("hello im racist");
-    return 0;
-}
--- Documents/c projects/testing/InternalPointer.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-main1(){
-    printf("hello world");
-    return 0;
-}
